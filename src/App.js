fafdaebdfba27f711f71e5a71549e48a369c43f8
Sixeye <srinath@srinath.fr>
2020-12-27 15:38:00 +0100

    firebase

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import React, { Component } from 'react'
+import React, { Component, createRef } from 'react'
 import './App.css'
 
 import Formulaire from './components/Formulaire'
 import Message from "./components/Message";
+
+// Firebase
+import base from './base'
 
 class App extends Component {
     state = {
@@ -10,9 +13,29 @@ class App extends Component {
         pseudo: this.props.match.params.pseudo
     }
 
+    messagesRef = createRef()
+
+    componentDidMount() {
+        base.syncState('/', {
+            context: this,
+            state: 'messages'
+        })
+    }
+
+    componentDidUpdate() {
+        const ref = this.messagesRef.current
+        ref.scrollTop = ref.scrollHeight
+    }
+
     addMessage = message => {
         const messages = { ...this.state.messages }
         messages[`message-${Date.now()}`] = message
+        Object
+            .keys(messages)
+            .slice(0, -10)
+            .forEach(key => {
+                messages[key] = null
+            })
         this.setState({messages})
     }
 
@@ -28,7 +51,7 @@ class App extends Component {
     return (
       <div className='box'>
         <div>
-          <div className='messages'>
+          <div className='messages' ref={this.messagesRef}>
             <div className="message">
                 { messages }
             </div>
